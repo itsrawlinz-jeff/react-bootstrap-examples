b56dd5c6e0ee892fab6aff81f064f24f750ad573
ChrisAchinga <achinga.chris@gmail.com>
2021-02-23 15:24:02 +0300

    update content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import { Container } from 'react-bootstrap'
 
 const Header = ({ pageTitle, pageDescription }) => {
-  const description = 'Feel free to clone and start using the template as yours'
+  const description = 'Bootstrap Examples in Create-React-App'
   return (
     <Container>
       <div className='starter-template text-center mt-5'>
-        <h1>{pageTitle} Page</h1>
+        <h1>{pageTitle}</h1>
         <p className='lead text-capitalize'>{pageDescription}</p>
         <p className='lead text-capitalize'>{description}</p>
       </div>
